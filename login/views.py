b6960c0957dcf75299e1024410754914c558353d
dkenna <dingo.mckenna@gmail.com>
2018-11-07 23:14:22 +0100

    moved files

--- login/views.py
+++ login/views.py
@@ -3,20 +3,17 @@ from login.models import Profile
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
-class ProfileSerializer(serializers.ModelSerializer):
-    user = serializers.HyperlinkedRelatedField(
-        many=False,
-        read_only=True,
-        view_name='user-detail'
-    )
-    class Meta:
-        model = Profile
-        fields = ('user', 'public_key')
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    #user = serializers.HyperlinkedRelatedField( many=False, read_only=True, view_name='user-detail')
+    user = UserSerializer(many=False, read_only=True)
+    class Meta:
+        model = Profile
+        fields = ('public_key','user')
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
