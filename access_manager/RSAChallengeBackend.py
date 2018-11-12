f18f4e3dfed743d5ad91fd7acb0fea490e8ff219
dkenna <dingo.mckenna@gmail.com>
2018-11-12 22:15:40 +0100

    draft backend

---
+++ access_manager/RSAChallengeBackend.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.models import User
+from token_generator import Challenge, SignedChallengeVerifier
+from jwt.exceptions import InvalidSignatureError
+
+class RSAChallengeBackend:
+    """
+    
+    """
+
+    def authenticate(self, request, username, signed_challenge):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return None
+        try:
+            verifier = SignedChallengeVerifier()
+            decoded = verifier.verify(username,signed_challenge)
+        except InvalidSignatureError as e:
+            """log some shit here"""
+            return None
+        return user
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
